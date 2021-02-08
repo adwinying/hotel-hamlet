cc77e8725af3bb2b258b2b643e2d0c06f248ca64
Adwin Ying <adwinying@gmail.com>
2021-02-09 02:06:21 +0900

    Init tailwindcss

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-xl">Hello from Login</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Login',
+})
+</script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -18,5 +18,7 @@ mix.webpackConfig({
 })
 
 mix.js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css', [])
+  .postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+  ])
   .vue();
