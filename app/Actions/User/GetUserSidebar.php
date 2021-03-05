b4f9b2f388ec8cf00359a98249fb0e4b2bf6a8ca
Adwin Ying <adwinying@gmail.com>
2021-03-06 00:55:45 +0900

    Create compose sidebar logic

---
+++ app/Actions/User/GetUserSidebar.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\User;
+
+use App\Models\User;
+
+class GetUserSidebar
+{
+    /**
+     * Get user's sidebar items
+     * @param User $user Target user
+     */
+    public function __invoke(User $user): array
+    {
+        return [
+            [
+                'name'   => 'Dashboard',
+                'url'    => '/admin',
+                'imgSrc' => '/img/icons/home.svg',
+            ],
+            [
+                'name'   => 'Hotels',
+                'url'    => '/admin/hotels',
+                'imgSrc' => '/img/icons/hotel.svg',
+            ],
+            [
+                'name'   => 'Rooms',
+                'url'    => '/admin/rooms',
+                'imgSrc' => '/img/icons/key.svg',
+            ],
+            [
+                'name'   => 'Reservations',
+                'url'    => '/admin/reservations',
+                'imgSrc' => '/img/icons/book.svg',
+            ],
+        ];
+    }
+}
