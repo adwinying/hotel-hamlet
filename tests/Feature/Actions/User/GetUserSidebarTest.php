9b1f229c3a6ee2e5df0ddf5b79a6b7669e699542
Adwin Ying <adwinying@gmail.com>
2021-03-06 00:55:45 +0900

    Create compose sidebar logic

---
+++ tests/Feature/Actions/User/GetUserSidebarTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Actions\User;
+
+use App\Actions\User\GetUserSidebar;
+use App\Models\User;
+use Tests\TestCase;
+
+class GetUserSidebarTest extends TestCase
+{
+    public function testGetListOfSidebarItems()
+    {
+        $user = new User();
+
+        $get    = app(GetUserSidebar::class);
+        $result = $get($user);
+
+        $expected = [
+            [
+                'name'   => 'Dashboard',
+                'url'    => '/admin',
+                'imgSrc' => '/img/icons/home.svg',
+            ],
+            [
+                'name'   => 'Hotels',
+                'url'    => '/admin/hotels',
+                'imgSrc' => '/img/icons/hotel.svg',
+            ],
+            [
+                'name'   => 'Rooms',
+                'url'    => '/admin/rooms',
+                'imgSrc' => '/img/icons/key.svg',
+            ],
+            [
+                'name'   => 'Reservations',
+                'url'    => '/admin/reservations',
+                'imgSrc' => '/img/icons/book.svg',
+            ],
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+}
