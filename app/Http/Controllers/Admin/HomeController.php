5ab6a44ca7e8a6e022ada27fba39b4e206a9cd12
Adwin Ying <adwinying@gmail.com>
2021-03-06 20:15:15 +0900

    Init dashboard page

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function show()
     {
-        return 'HOME';
+        return Inertia::render('Dashboard/Index');
     }
 }
---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    dashboard
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {
+    }
+  },
+})
+</script>
