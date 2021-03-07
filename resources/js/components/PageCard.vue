27c0e68a217428bf73f12563deb914909cbd13ae
Adwin Ying <adwinying@gmail.com>
2021-03-07 14:29:05 +0900

    Init PageCard component

---
+++ resources/js/components/PageCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full p-4 bg-white rounded shadow-lg">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageCard',
+})
+</script>
