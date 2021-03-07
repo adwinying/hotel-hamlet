4444272cbbefedba64e51450263d3762dae4775f
Adwin Ying <adwinying@gmail.com>
2021-03-07 12:30:01 +0900

    Refactor layout

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-    dashboard
-  </div>
+  <page title="Dashboard">
+  </page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
+  name: 'DashboardIndex',
+
   setup() {
     return {
     }
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,27 +2,24 @@ import { createApp, h } from 'vue'
 import { App, plugin } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 
-import Layout from './components/Layout.vue'
+import Page from './components/Page.vue'
 
 const el = document.getElementById('app')
 
-createApp({
+const app = createApp({
   render: () => h(App, {
     initialPage: JSON.parse(el?.dataset.page ?? ''),
     resolveComponent: (name: string) => import(`./Pages/${name}`)
-      .then(({ default: page }) => {
-        if (page.layout === undefined) {
-          // eslint-disable-next-line no-param-reassign
-          page.layout = Layout
-        }
-
-        return page
-      }),
+      .then((module) => module.default),
   }),
 })
-  .use(plugin)
-  .mount(el ?? '#app')
+
+app.use(plugin)
+app.mount(el ?? '#app')
+app.component('Page', Page)
 
 InertiaProgress.init({
   showSpinner: true,
 })
+
+export default app
