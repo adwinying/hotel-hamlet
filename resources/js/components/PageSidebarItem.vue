6ad6d442385df235e5b917724cd4f1e2c21393a8
Adwin Ying <adwinying@gmail.com>
2021-03-07 23:38:25 +0900

    Extract sidebar item into new component

---
+++ resources/js/components/PageSidebarItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <inertia-link
+      :href="url"
+      :class="{ 'bg-gray-200': isActive }"
+      class="flex items-center p-3 space-x-2 rounded font-bold hover:bg-cyan-100">
+      <img
+        :src="imgSrc"
+        :alt="name"
+        class="w-8">
+      <span>
+        {{ name }}
+      </span>
+    </inertia-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageSidebarItem',
+
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+
+    url: {
+      type: String,
+      required: true,
+    },
+
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
