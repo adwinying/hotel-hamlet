9ccb49f42f87f107717a961fcd32e84fa46ed72e
Adwin Ying <adwinying@gmail.com>
2021-03-07 13:00:21 +0900

    Init PageTitle component

---
+++ resources/js/components/PageTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="w-full pt-12 pb-32 bg-cyan-500">
+    <div class="container mx-auto px-10">
+      <h1 class="font-bold text-5xl">
+        <slot />
+      </h1>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageTitle',
+})
+</script>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,7 +15,7 @@
   <meta name="theme-color" content="#ffffff">
 
   <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Gabriela&family=Open+Sans&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Gabriela&family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
   <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
   <script src="{{ mix('/js/app.js') }}" defer></script>
   <title>Hotel Hamlet Admin Dashboard</title>
