58c6831cc06a060b82aebbaf2731921178ab9b8a
Adwin Ying <adwinying@gmail.com>
2021-03-29 01:09:10 +0900

    Init update user action

---
+++ app/Actions/User/UpdateUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\User;
+
+use App\Models\User;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+
+class UpdateUser
+{
+    /**
+     * Update user info
+     * @param User  $user  Target user
+     * @param array $input Data to update
+     */
+    public function __invoke(User $user, array $input): bool
+    {
+        // Hash password before saving to DB
+        if ($password = Arr::pull($input, 'password')) {
+            $input['password'] = Hash::make($password);
+        }
+
+        return $user->update($input);
+    }
+}
