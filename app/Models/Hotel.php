5ebbac90afe03298f2b8267e66106ebb820c94c1
Adwin Ying <adwinying@gmail.com>
2021-04-10 22:19:07 +0900

    Init hotel DB table

---
+++ app/Models/Hotel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'is_hidden',
+    ];
+}
