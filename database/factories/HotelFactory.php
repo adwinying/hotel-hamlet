757bfc89f3da4c5a17e86e6fb9cfd6ae15d13e30
Adwin Ying <adwinying@gmail.com>
2021-04-10 22:19:07 +0900

    Init hotel DB table

---
+++ database/factories/HotelFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Hotel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class HotelFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Hotel::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'      => 'Hotel' . Str::ucfirst($this->faker->word),
+            'is_hidden' => false,
+        ];
+    }
+}
---
+++ database/seeders/HotelSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Hotel;
+use Illuminate\Database\Seeder;
+
+class HotelSeeder extends Seeder
+{
+    const AVAILABLE_HOTELS = [
+        'Hotel Hamlet City',
+        'Hotel Hamlet Beachside',
+        'Hotel Hamlet Ski Resort',
+        'Hotel Hamlet Campgrounds',
+    ];
+
+    const UNAVAILABLE_HOTELS = [
+        'Hotel Hamlet Airport Express',
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (self::AVAILABLE_HOTELS as $hotelName) {
+            Hotel::factory()->create([
+                'name'      => $hotelName,
+                'is_hidden' => false,
+            ]);
+        }
+
+        foreach (self::UNAVAILABLE_HOTELS as $hotelName) {
+            Hotel::factory()->create([
+                'name'      => $hotelName,
+                'is_hidden' => true,
+            ]);
+        }
+    }
+}
