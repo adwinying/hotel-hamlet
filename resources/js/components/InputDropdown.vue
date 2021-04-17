82015b4e1ae66a1527874836bed8b85ad50b2bd3
Adwin Ying <adwinying@gmail.com>
2021-04-17 23:20:52 +0900

    Init input dropdown component

--- resources/js/components/InputDropdown.vue
+++ resources/js/components/InputDropdown.vue
@@ -6,16 +6,23 @@
       {{ label }}
     </label>
 
-    <input
+    <select
       :id="name"
       v-model="value"
       v-bind="$attrs"
-      :type="type"
       :name="name"
       :class="errors ? 'border-red-500' : 'border-gray-300'"
-      class="block w-full mt-1 rounded-md shadow-sm
-        focus:ring-cyan-500 focus:border-cyan-600 disabled:bg-gray-200
-        sm:text-sm">
+      class="block w-full mt-1 py-2 px-3 border border-gray-300
+      bg-white rounded-md shadow-sm disabled:bg-gray-200
+      focus:outline-none focus:ring-cyan-500 focus:border-cyan-600
+      sm:text-sm">
+      <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value">
+        {{ option.label || option.value }}
+      </option>
+    </select>
 
     <div
       v-if="errors"
@@ -26,15 +33,16 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import InputData from '../types/InputData'
+import DropdownOption from '../types/DropdownOption'
 
 export default defineComponent({
-  name: 'InputText',
+  name: 'InputDropdown',
 
   props: {
     modelValue: {
-      type: [String, Number],
+      type: [String, Number, Boolean],
       default: '',
     },
 
@@ -48,9 +56,9 @@ export default defineComponent({
       required: true,
     },
 
-    type: {
-      type: String,
-      default: 'text',
+    options: {
+      type: Object as PropType<DropdownOption[]>,
+      required: true,
     },
 
     errors: {
