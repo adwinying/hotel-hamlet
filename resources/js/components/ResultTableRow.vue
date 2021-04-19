fdcbbaee37aa6b8f0ceabcd769900fbd0afc1821
Adwin Ying <adwinying@gmail.com>
2021-04-19 23:39:19 +0900

    Init result table components

---
+++ resources/js/components/ResultTableRow.vue
@@ -0,0 +1,13 @@
+<template>
+  <tr class="hover:bg-gray-100 focus:bg-gray-100">
+    <slot />
+  </tr>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ResultTableRow',
+})
+</script>
---
+++ resources/js/components/ResultTableWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white border rounded-md shadow overflow-x-auto">
+    <table class="w-full">
+      <slot />
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ResultTableWrapper',
+})
+</script>
