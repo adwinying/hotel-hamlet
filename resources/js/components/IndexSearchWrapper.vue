72db046b1f664e235dd663574fa5142edd6ce275
Adwin Ying <adwinying@gmail.com>
2021-04-21 00:18:42 +0900

    Init IndexSearchWrapper component

---
+++ resources/js/components/IndexSearchWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex space-x-3 mb-6">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexSearchWrapper',
+})
+</script>
