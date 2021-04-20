da86aaadb606093ac7e29d1b3631c956e2527972
Adwin Ying <adwinying@gmail.com>
2021-04-21 00:22:50 +0900

    Populate Hotel index page with actual data

--- resources/js/Pages/Hotel/Index.vue
+++ resources/js/Pages/Hotel/Index.vue
@@ -1,23 +1,102 @@
 <template>
   <page title="Hotels">
-    hello from hotels
+    <index-search-wrapper>
+      <input-text
+        v-model="searchParams.name"
+        name="name"
+        label="Name"
+        class="w-72" />
+
+      <input-dropdown
+        v-model="searchParams.is_hidden"
+        name="is_hidden"
+        :options="hiddenOptions"
+        label="Is Hidden?"
+        class="w-72" />
+    </index-search-wrapper>
+
+    <result-table
+      :fields="fields"
+      :formatter="formatter"
+      :data="result.data" />
   </page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ResultTableField from '../../types/ResultTableField'
+import DropdownOption from '../../types/DropdownOption'
+import useIndexSearch from '../../composables/useIndexSearch'
+
+import IndexSearchWrapper from '../../components/IndexSearchWrapper.vue'
 import InputText from '../../components/InputText.vue'
-import LoadingButton from '../../components/LoadingButton.vue'
+import InputDropdown from '../../components/InputDropdown.vue'
+import ResultTable from '../../components/ResultTable.vue'
 
 export default defineComponent({
   name: 'ProfileIndex',
 
   components: {
+    IndexSearchWrapper,
     InputText,
-    LoadingButton,
+    InputDropdown,
+    ResultTable,
+  },
+
+  props: {
+    query: {
+      type: Object,
+      required: true,
+    },
+
+    result: {
+      type: Object,
+      required: true,
+    },
   },
 
   setup(props) {
+    const fields: ResultTableField[] = [
+      {
+        key: 'name',
+        label: 'Name',
+      },
+      {
+        key: 'is_hidden',
+        label: 'Is Hidden?',
+      },
+    ]
+
+    const formatter = {
+      is_hidden: (data: boolean) => (data ? 'Yes' : 'No'),
+    }
+
+    const hiddenOptions: DropdownOption[] = [
+      {
+        value: '',
+        label: '',
+      },
+      {
+        value: 1,
+        label: 'Yes',
+      },
+      {
+        value: 0,
+        label: 'No',
+      },
+    ]
+
+    const { searchParams } = useIndexSearch(props.query, {
+      is_hidden: '',
+      name: '',
+    })
+
+    return {
+      fields,
+      searchParams,
+      formatter,
+      hiddenOptions,
+    }
   },
 })
 </script>
