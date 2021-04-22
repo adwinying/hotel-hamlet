9644bd1756062195c44bd1e9e43e5c254060f6e7
Adwin Ying <adwinying@gmail.com>
2021-04-22 23:42:27 +0900

    Implement hotel index filter tests

--- tests/Feature/HotelIndexTest.php
+++ tests/Feature/HotelIndexTest.php
@@ -28,4 +28,71 @@ class HotelIndexTest extends TestCase
                 ->has('is_hidden'))
             ->where('result.data', $hotels));
     }
+
+    public function testCanFilterByName()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $hotels = Hotel::factory()->count(3)->create()
+            ->map
+            ->only('id', 'name', 'is_hidden');
+
+        $hotel = $hotels->random();
+
+        $this->get("/admin/hotels?name={$hotel['name']}")
+             ->assertInertia(
+                 fn (Assert $page) => $page
+                    ->component('Hotel/Index')
+                    ->has('query', fn (Assert $query) => $query
+                        ->has('name')
+                        ->has('is_hidden'))
+                        ->where('result.data', [$hotel])
+             );
+    }
+
+    public function testCanFilterByIsHidden()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $hotels = Hotel::factory()->count(3)->create()
+            ->map
+            ->only('id', 'name', 'is_hidden');
+
+        $isHidden = boolval(mt_rand(0, 1));
+        $hotel    = $hotels->where('is_hidden', $isHidden);
+
+        $this->get("/admin/hotels?is_hidden=$isHidden")
+             ->assertInertia(
+                 fn (Assert $page) => $page
+                    ->component('Hotel/Index')
+                    ->has('query', fn (Assert $query) => $query
+                        ->has('name')
+                        ->has('is_hidden'))
+                        ->where('result.data', $hotel->toArray())
+             );
+    }
+
+    public function testCannotFilterById()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $hotels = Hotel::factory()->count(3)->create()
+            ->map
+            ->only('id', 'name', 'is_hidden');
+
+        $hotel = $hotels->random();
+
+        $this->get("/admin/hotels?id={$hotel['id']}")
+             ->assertInertia(
+                 fn (Assert $page) => $page
+                    ->component('Hotel/Index')
+                    ->has('query', fn (Assert $query) => $query
+                        ->has('name')
+                        ->has('is_hidden'))
+                        ->where('result.data', $hotels)
+             );
+    }
 }
