69bf988efe8f03d9b9c4a0e019f2204d9508d2c8
Adwin Ying <adwinying@gmail.com>
2021-04-24 23:22:41 +0900

    Init hotel edit page

--- resources/js/Pages/Hotel/Form.vue
+++ resources/js/Pages/Hotel/Form.vue
@@ -1,23 +1,91 @@
 <template>
-  <page title="Hotel Form">
-    hello from hotel form
+  <page :title="pageTitle">
+    <form @submit.prevent="onFormSubmit">
+      <div class="grid grid-cols-6 gap-6">
+        <input-text
+          v-model="form.name"
+          name="name"
+          label="Name"
+          class="lg:col-span-4" />
+
+        <input-checkbox
+          v-model="form.is_hidden"
+          name="is_hidden"
+          label="Is Hidden?"
+          class="lg:col-span-4" />
+      </div>
+
+      <loading-button
+        class="mt-6"
+        type="submit"
+        :is-loading="form.loading">
+        {{ submitText }}
+      </loading-button>
+    </form>
   </page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
+import Hotel from '../../types/Models/Hotel'
+import { showToast } from '../../composables/useAlert'
+import useForm from '../../composables/useForm'
+
+import InputText from '../../components/InputText.vue'
+import InputCheckbox from '../../components/InputCheckbox.vue'
+import LoadingButton from '../../components/LoadingButton.vue'
 
 export default defineComponent({
   name: 'HotelForm',
 
+  components: {
+    InputText,
+    InputCheckbox,
+    LoadingButton,
+  },
+
   props: {
     hotel: {
-      type: Object,
-      required: true,
+      type: Object as PropType<Hotel | null>,
+      default: null,
     },
   },
 
-  setup() {
+  setup(props) {
+    const initialFormData = {
+      name: '',
+      is_hidden: false,
+    }
+
+    const {
+      form,
+      objectId: hotelId,
+      isEditForm,
+    } = useForm(props.hotel, initialFormData)
+
+    const pageTitle = computed(() => (
+      isEditForm.value ? 'Edit Hotel' : 'New Hotel'
+    ))
+
+    const submitText = computed(() => (
+      isEditForm.value ? 'Update Hotel' : 'Create Hotel'
+    ))
+
+    const onFormSubmit = () => {
+      form.clearErrors()
+      form.post(`/admin/hotel/${hotelId.value}`, {
+        onSuccess: () => {
+          showToast('Hotel successfully updated', 'success')
+        },
+      })
+    }
+
+    return {
+      pageTitle,
+      submitText,
+      form,
+      onFormSubmit,
+    }
   },
 })
 </script>
