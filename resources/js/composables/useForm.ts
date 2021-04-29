843d47a56f971769d80395950c91a4e099c25f3b
Adwin Ying <adwinying@gmail.com>
2021-04-30 00:29:13 +0900

    Update inertia-vue3 package

--- resources/js/composables/useForm.ts
+++ resources/js/composables/useForm.ts
@@ -1,10 +1,10 @@
-import { computed, ComputedRef, UnwrapRef } from 'vue'
-import { FormWithData, useForm as useInertiaForm } from '@inertiajs/inertia-vue3'
+import { computed, ComputedRef } from 'vue'
+import { InertiaForm, useForm as useInertiaForm } from '@inertiajs/inertia-vue3'
 import Model from '../types/Models/Model'
 import FormData from '../types/FormData'
 
 interface UseForm {
-  form: UnwrapRef<FormWithData<FormData>>
+  form: InertiaForm<FormData>
   objectId: ComputedRef<number | null>
   isEditForm: ComputedRef<boolean>
 }
