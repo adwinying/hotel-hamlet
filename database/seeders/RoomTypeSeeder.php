2d1711a71d0c9fa283797dded809e707a34c6dd5
Adwin Ying <adwinying@gmail.com>
2021-05-07 15:32:29 +0900

    Init room type table

---
+++ database/seeders/RoomTypeSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Hotel;
+use App\Models\RoomType;
+use Illuminate\Database\Seeder;
+
+class RoomTypeSeeder extends Seeder
+{
+    const ROOM_TYPES = [
+        'Single',
+        'Deluxe',
+        'Suite',
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $hotels = Hotel::count()
+            ? Hotel::all()->random(5)
+            : Hotel::factory()->count(5)->create();
+
+        foreach ($hotels as $hotel) {
+            foreach (self::ROOM_TYPES as $types) {
+                RoomType::factory()->create([
+                    'hotel_id' => $hotel->id,
+                    'name'     => $types,
+                ]);
+            }
+        }
+    }
+}
