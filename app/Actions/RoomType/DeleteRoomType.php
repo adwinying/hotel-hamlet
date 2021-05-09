e86ec5a76f47af324f42cf30c5a9160acd95e5f1
Adwin Ying <adwinying@gmail.com>
2021-05-09 21:03:56 +0900

    Init room type delete action

---
+++ app/Actions/RoomType/DeleteRoomType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Actions\RoomType;
+
+use App\Models\RoomType;
+
+class DeleteRoomType
+{
+    /**
+     * Delete room type
+     * @param RoomType $roomType Room type to be deleted
+     */
+    public function execute(RoomType $roomType): bool
+    {
+        return $roomType->delete();
+    }
+}
