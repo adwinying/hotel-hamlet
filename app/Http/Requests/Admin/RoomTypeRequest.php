451e8c6b8ab6a475098e188f3d4a2ab03197a00b
Adwin Ying <adwinying@gmail.com>
2021-05-09 21:25:27 +0900

    Init room type validation rules

---
+++ app/Http/Requests/Admin/RoomTypeRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class RoomTypeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'hotel_id' => 'required|exists:hotels,id',
+            'name'     => [
+                'required',
+                'max:255',
+                Rule::unique('room_types')
+                    ->where(fn ($q) => $q->whereHotelId($this->input('hotel_id')))
+                    ->ignore($this->route('room_type')),
+            ],
+        ];
+    }
+
+    /**
+     * Get the attributes for the defined validation rules.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'hotel_id' => 'Hotel',
+        ];
+    }
+}
---
+++ tests/Feature/Requests/Admin/RoomTypeRequestTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature\Requests\Admin;
+
+use App\Http\Requests\Admin\RoomTypeRequest;
+use App\Models\Hotel;
+use App\Models\RoomType;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+use Tests\ValidationTestCase;
+
+class RoomTypeRequestTest extends ValidationTestCase
+{
+    use RefreshDatabase;
+
+    protected $hotelId      = 99;
+    protected $roomTypeName = 'foobar';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Hotel::factory()->create([
+            'id' => $this->hotelId,
+        ]);
+
+        RoomType::factory()->create([
+            'hotel_id' => $this->hotelId,
+            'name'     => $this->roomTypeName,
+        ]);
+    }
+
+    protected function request(): FormRequest
+    {
+        return new RoomTypeRequest();
+    }
+
+    protected function baseInput(): array
+    {
+        return [
+            'hotel_id' => $this->hotelId,
+            'name'     => Str::random(255),
+        ];
+    }
+
+    public function formData()
+    {
+        return [
+            'All OK' => [
+                true,
+            ],
+
+            'hotel_id is empty' => [
+                false, [], 'hotel_id',
+            ],
+
+            'hotel_id does not exist in DB' => [
+                false, ['hotel_id' => $this->hotelId + 1],
+            ],
+
+            'name is empty' => [
+                false, [], 'name',
+            ],
+
+            'name exceeds 255 chars' => [
+                false, ['name' => Str::random(256)],
+            ],
+
+            'name + hotel_id combo is not unique' => [
+                false, ['name' => $this->roomTypeName],
+            ],
+        ];
+    }
+}
