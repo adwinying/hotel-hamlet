fb25c7e54df6ff07bbc8886d2e0d5256ced10aa9
Adwin Ying <adwinying@gmail.com>
2021-05-09 21:27:52 +0900

    Init room type update action

---
+++ app/Actions/RoomType/UpdateRoomType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Actions\RoomType;
+
+use App\Models\RoomType;
+
+class UpdateRoomType
+{
+    /**
+     * Update room type
+     * @param RoomType $roomType Room type to be updated
+     * @param array    $params   Data to update
+     * @return App\Models\RoomType
+     */
+    public function execute(RoomType $roomType, array $input = []): RoomType
+    {
+        $roomType->update($input);
+
+        return $roomType;
+    }
+}
