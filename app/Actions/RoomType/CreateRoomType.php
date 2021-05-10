91630e1e1934ae88b2cfd17d11b1e25d60884274
Adwin Ying <adwinying@gmail.com>
2021-05-10 20:49:53 +0900

    Init room type create action

---
+++ app/Actions/RoomType/CreateRoomType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Actions\RoomType;
+
+use App\Models\RoomType;
+
+class CreateRoomType
+{
+    /**
+     * Create room type
+     * @param array $params Data to create
+     * @return App\Models\RoomType
+     */
+    public function execute(array $input = []): RoomType
+    {
+        return RoomType::create($input);
+    }
+}
---
+++ tests/Feature/Actions/RoomType/CreateRoomTypeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Actions\RoomType;
+
+use App\Actions\RoomType\CreateRoomType;
+use App\Models\RoomType;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateRoomTypeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testCreatesOnlyOneRoomType()
+    {
+        $input = RoomType::factory()->make()->toArray();
+
+        $create = app(CreateRoomType::class);
+        $result = $create->execute($input);
+
+        $this->assertEquals($input['hotel_id'], $result->hotel_id);
+        $this->assertEquals($input['name'], $result->name);
+        $this->assertDatabaseHas('room_types', [
+            'hotel_id' => $input['hotel_id'],
+            'name'     => $input['name'],
+        ]);
+    }
+}
