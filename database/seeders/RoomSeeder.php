d69d7d5142e2c972b98d3e499a5973356b601854
Adwin Ying <adwinying@gmail.com>
2021-05-12 23:59:17 +0900

    Init room DB table

---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Room;
+use App\Models\RoomType;
+use Illuminate\Database\Seeder;
+
+class RoomSeeder extends Seeder
+{
+    const ROOM_NO_MAPPING = [
+        'Single' => 200,
+        'Deluxe' => 300,
+        'Suite'  => 400,
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roomTypes = RoomType::count()
+            ? RoomType::all()
+            : RoomType::factory()->count(5)->create();
+
+        foreach ($roomTypes as $roomType) {
+            foreach (range(1, 10) as $i) {
+                $roomNoPrefix = self::ROOM_NO_MAPPING[$roomType->name] ?? 100;
+
+                Room::factory()->create([
+                    'room_type_id' => $roomType->id,
+                    'room_no'      => $roomNoPrefix + $i,
+                ]);
+            }
+        }
+    }
+}
