6851e129cad59ebb8c4214bd7c990be9d0664849
Adwin Ying <adwinying@gmail.com>
2021-05-13 00:14:31 +0900

    Init filter room action

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Actions\Room\FilterRoom;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -20,6 +22,11 @@ class Room extends Model
         'room_type_id',
         'room_no',
     ];
+
+    public function scopeFilter(Builder $query, array $params)
+    {
+        return app(FilterRoom::class)->execute($query, $params);
+    }
 
     public function roomType()
     {
