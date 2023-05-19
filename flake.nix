5e932f8d4343653b72b86ebfcc3894b9041315ec
Adwin Ying <adwinying@gmail.com>
2023-05-20 02:47:44 +0900

    Define build command

--- flake.nix
+++ flake.nix
@@ -9,21 +9,37 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      basePkgs = with pkgs; [
+        envsubst
+        nginx
+        php82
+        php82Packages.composer
+        nodejs_16
+      ];
     in {
-      devShell = with pkgs; mkShell {
-        buildInputs = [
-          envsubst
-          nginx
-          php82
-          php82Packages.composer
-          nodejs_16
-        ];
+      defaultPackage = pkgs.stdenv.mkDerivation {
+        name = "hotel-hamlet";
+        src = builtins.path { path = ./.; name = "src"; };
+        buildInputs = basePkgs ++ [ pkgs.git pkgs.curl ];
 
         shellHook = ''
           echo "`${pkgs.php82}/bin/php --version`"
           echo "`${pkgs.php82Packages.composer}/bin/composer --version`"
           echo "node: `${pkgs.nodejs_16}/bin/node --version`"
           echo "npm: v`${pkgs.nodejs_16}/bin/npm --version`"
+        '';
+
+        buildPhase = ''
+          #composer install --optimize-autoloader --no-cache --no-interaction --no-scripts --prefer-dist
+          ls -la
+          php artisan
+          php artisan typescript:transform --force --output js/generated.d.ts
+          ASSET_CMD="build" npm run build
+        '';
+
+        installPhase = ''
+          mkdir -p $out/src
+          cp -r . $out/src
         '';
       };
     });
