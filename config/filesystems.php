6eb6ca6d015245b6955c4b8470cb274adedeb315
Adwin Ying <adwinying@gmail.com>
2024-01-31 01:26:39 +0900

    Set a random image for header page title

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,10 +34,8 @@ return [
         ],
 
         'public' => [
-            'driver'     => 'local',
-            'root'       => storage_path('app/public'),
-            'url'        => env('APP_URL') . '/storage',
-            'visibility' => 'public',
+            'driver' => 'local',
+            'root'   => public_path(''),
         ],
 
         's3' => [
